2cf8d34857fd41d25230f9a189f3f2b3b1c1f7da
João Pedro Azevedo <joaopzevedo@gmail.com>
2025-06-17 20:22:44 -0300

    Feat: Create Theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { lightTheme,  GlobalStyle, darkTheme } from './styles/'; 
+import { /*lightTheme,*/  GlobalStyle, darkTheme } from './styles/'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
       <React.StrictMode>
