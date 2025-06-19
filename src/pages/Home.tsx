ad02c42d6af80f00d8a8368b6120b143c35e8d5d
João Pedro Azevedo <joaopzevedo@gmail.com>
2025-06-18 23:39:06 -0300

    Feat: Cards Components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
-import { Header } from "@/componnents";
+import { Header, CardComponent } from "@/componnents";
+import { Container } from "@mui/material";
 
 function Home() {
   return (
     <>
       <Header />
+      <Container maxWidth="lg">
+        <CardComponent />
+        <CardComponent className='warning'>
+          Card
+        </CardComponent>
+      </Container>
     </>
   );
 }
