924ad1dd19b709e9a9b984f4f7e432c83ff4819c
João Pedro Azevedo <joaopzevedo@gmail.com>
2025-06-19 00:55:52 -0300

    Feat: Avatar List

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,33 @@
-import { Header, CardComponent } from "@/componnents";
+import { Header, CardComponent, AvatarList } from "@/componnents";
 import { Container } from "@mui/material";
+import { currencyConverter } from "@/utils";
 
 function Home() {
+  const mockListData = [
+    {
+      avatar:'/avatarMock1.svg',
+      name: '1 - Nome e Sobrenome',
+      subtitle: currencyConverter(1000),
+    },
+    {
+      avatar:'/avatarMock1.svg',
+      name: '2 - Nome e Sobrenome',
+      subtitle:currencyConverter(2000),
+    },
+    {
+      avatar:'/avatarMock1.svg',
+      name: '3 - Nome e Sobrenome',
+      subtitle:currencyConverter(3000),
+    },
+  ]
+  
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <CardComponent />
-        <CardComponent className='warning'>
-          Card
+        
+        <CardComponent >
+          <AvatarList listData={mockListData} />
         </CardComponent>
       </Container>
     </>
