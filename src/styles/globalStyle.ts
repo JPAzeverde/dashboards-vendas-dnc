4e8d9e9c7c890560d72df581a0d9be1504505840
João Pedro Azevedo <joaopzevedo@gmail.com>
2025-06-25 15:20:27 -0300

    feat : homepage requests

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -13,6 +13,10 @@ export const GlobalStyle = createGlobalStyle<{ theme?:  DefaultTheme}>`
     h1, h2, h3, h4, h5, h6, ul, li, p, figure{
         margin: 0; padding: 0;
     }
+    a{
+        text-decoration:none;
+        color: ${(props) => props.theme.appColor};
+    }
     
     .mb-1{
         margin-bottom: ${pxToRem(16)};
@@ -21,4 +25,28 @@ export const GlobalStyle = createGlobalStyle<{ theme?:  DefaultTheme}>`
     .mb-2{
         margin-bottom: ${pxToRem(32)};
     }
+
+    skeleton-loading{
+        animation: skeletonLoading 2s infinite alternate;
+    }
+
+    @keyframes skeletonLoading {
+        from{
+            background-color:${(props) => props.theme.appSkeletonFrom};
+        }
+        to{
+            background-color:${(props) => props.theme.appSkeletonTo};
+        }
+    }
+    
+    skeletonLoading-mh1{
+        min-height:${pxToRem(175)};;
+    }
+        
+    skeletonLoading-mh2{
+        min-height:${pxToRem(400)};;
+    }
+
+
+
 `;
